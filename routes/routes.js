c7a4c22344f0ae2d5a4e0b0cafb6df3d23d39af2
joshosas <joshuaosaro20@gmail.com>
2023-07-19 21:20:58 +0100

    preparation for interview

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const User = require("../models/users");
 // const User = require("../models/user_info");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const GLOBAL = [];
 
 // GET Route for Home
 router.get("/", (req, res) => {
@@ -26,16 +27,16 @@ router.post("/signup", (req, res) => {
     lname: req.body.lname,
     email: req.body.email,
     password: req.body.password,
-    // OTHERS
-    // MORNING
+    // // OTHERS
+    // // MORNING
     am4_6: req.body.am4_6 || "Eat, Code, Sleep",
     am6_9: req.body.am6_9 || "Eat, Code, Sleep",
     am9_12pm: req.body.am9_12pm || "Eat, Code, Sleep",
-    // AFTERNOON
+    // // AFTERNOON
     pm12_3: req.body.pm12_3 || "Eat, Code, Sleep",
     pm3_6: req.body.pm3_6 || "Eat, Code, Sleep",
     pm6_9: req.body.pm6_9 || "Eat, Code, Sleep",
-    // NIGHT
+    // // NIGHT
     pm9_12am: req.body.pm9_12am || "Eat, Code, Sleep",
     am12_2: req.body.am12_2 || "Eat, Code, Sleep",
     am2_4: req.body.am2_4 || "Eat, Code, Sleep",
@@ -65,7 +66,7 @@ router.get("/signin", (req, res) => {
 router.post("/signin", async (req, res) => {
   try {
     const userExist = await User.findOne({ email: req.body.email });
-    console.log(userExist);
+    GLOBAL.push(userExist);
 
     if (userExist.password === req.body.password) {
       res.render("dashboard", { title: userExist.fname, userInfo: userExist });
@@ -79,14 +80,98 @@ router.post("/signin", async (req, res) => {
   }
 });
 
-// GET Route for Blog
-router.get("/blog", (req, res) => {
-  res.render("blog", { title: "BLOG" });
-});
-
 // GET Route for Dashboard
 router.get("/dashboard", (req, res) => {
-  res.redirect("/signin");
+  res.render("/dashboard");
 });
+
+// GET ROUTE FOR EDIT
+router.get("/edit_todos", (req, res) => {
+  userData = GLOBAL[0];
+  console.log(userData);
+  res.render("edit_todos", { title: "EDIT TODOS", userInfo: userData });
+});
+
+// Edit an user Todo
+router.post("/edit_todos", (req, res) => {
+  const user = new User({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    password: req.body.password,
+    //     // // OTHERS
+    //     // // MORNING
+    am4_6: req.body.am4_6,
+    am6_9: req.body.am6_9,
+    am9_12pm: req.body.am9_12pm,
+    //     // // AFTERNOON
+    pm12_3: req.body.pm12_3,
+    pm3_6: req.body.pm3_6,
+    pm6_9: req.body.pm6_9,
+    //     // // NIGHT
+    pm9_12am: req.body.pm9_12am,
+    am12_2: req.body.am12_2,
+    am2_4: req.body.am2_4,
+  });
+  user
+    .save()
+    .then(() => {
+      req.session.message = {
+        type: "success",
+        message: "User added successfully",
+      };
+      res.render("dashboard", { title: GLOBAL[0].fname, userInfo: GLOBAL[0] });
+    })
+    .catch((err) => {
+      res.json({ message: err.message, type: "danger" });
+      console.log(err);
+      res.redirect("/signup");
+    });
+});
+
+// router.post("/edit", async (req, res) => {
+//   try {
+//     const userExist = await User.findOne({ email: req.body.email });
+//     console.log(userExist._id);
+//     res.render("edit_todos", { title: userExist.fname, userInfo: userExist });
+//   } catch {
+//     res.send("<h2>Wrong email! Please go back and try again.</h2>");
+//   }
+// });
+
+// // Update an User Todo
+// router.post("/update", async (req, res) => {
+//   const userExist = await User.findOne({ email: req.body.email });
+//   const id = userExist._id;
+//   User.findOneAndUpdate(id, {
+//     // MORNING
+//     am4_6: req.body.am4_6,
+//     am6_9: req.body.am6_9,
+//     am9_12pm: req.body.am9_12pm,
+//     // AFTERNOON
+//     pm12_3: req.body.pm12_3 || "Eat, Code, Sleep",
+//     pm3_6: req.body.pm3_6 || "Eat, Code, Sleep",
+//     pm6_9: req.body.pm6_9 || "Eat, Code, Sleep",
+//     // NIGHT
+//     pm9_12am: req.body.pm9_12am || "Eat, Code, Sleep",
+//     am12_2: req.body.am12_2 || "Eat, Code, Sleep",
+//     am2_4: req.body.am2_4 || "Eat, Code, Sleep",
+//   });
+//   userExist
+//     .save()
+//     .then(() => {
+//       req.session.message = {
+//         type: "success",
+//         message: "User updated successfully",
+//       };
+//       res.redirect("/dashboard");
+//     })
+//     .catch((err) => {
+//       res.json({ message: err.message, type: "danger" });
+//       console.log(err);
+//       res.redirect("/signup");
+//     });
+//   res.render("dashboard", { title: userExist.fname, userInfo: userExist });
+// });
 
 module.exports = router;
