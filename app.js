4bdf7eccbd02ee2bd8afd1d2c3ecee854d7d8e87
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-07-21 21:42:23 +0200

    updated imports on the main app.js and added required routes and files 🔥🔥🔥😎
    Set templating engine and layout for the main.ejs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // imports
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+const expressLayout = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+
 const path = require("path");
+
 const { log, error } = require("console");
 
 const app = express();
@@ -21,6 +27,8 @@ db.once("open", () => console.log("database connected"));
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride("_method"));
 
 // Static Files
 app.use(express.static(path.join(__dirname, "public")));
@@ -40,9 +48,15 @@ app.use((req, res, next) => {
 });
 
 // Set Templating Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-app.use("", require("./routes/routes"));
+
+//app.use("", require("./routes/routes"));
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+
 
 app.listen(PORT, () => {
   console.log(`Server started http://localhost:${PORT}`);
